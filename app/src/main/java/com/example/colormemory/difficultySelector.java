4cb11725434f8f4132630f0ebcc25e21d87c3b07
NinoLS <belicnino77@gmail.com>
2021-02-10 14:41:20 +0100

    Add difficultySelector + difficultyFacile layout

---
+++ app/src/main/java/com/example/colormemory/difficultySelector.java
@@ -0,0 +1,14 @@
+package com.example.colormemory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class difficultySelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty_selector);
+    }
+}

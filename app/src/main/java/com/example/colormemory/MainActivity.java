e6698aab4547db1c0c78e4dec246665ed31b9e02
Efkat <hugo.ben80000@gmail.com>
2021-02-21 21:51:01 +0100

    Finish : Sign Up Layout

--- app/src/main/java/com/example/colormemory/MainActivity.java
+++ app/src/main/java/com/example/colormemory/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.colormemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 

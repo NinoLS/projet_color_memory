728ed763d48f95b2d890d3f28ed90e5a78c295c0
Efkat <hugo.ben80000@gmail.com>
2021-03-09 12:58:01 +0100

    Refactor : layout & Add : User properties

--- app/src/main/java/database/User.java
+++ app/src/main/java/database/User.java
@@ -1,37 +1,31 @@
 package database;
 
 
-import android.widget.DatePicker;
-
 import androidx.annotation.NonNull;
-
-import java.util.Date;
 
 public class User {
     private int idUser;
     private String name;
     private String password;
     private String birth;
+    private String email;
+    private byte gender;
 
-    public User(String _name, String _password, String _birth) {
-        this.name = _name;
-        this.password = _password;
-        this.birth = _birth;
+    public User(String _name, String _password, String _birth, String _email, byte _gender) {
+        this.setName(_name);
+        this.setPassword(_password);
+        this.setBirth(_birth);
+        this.setEmail(_email);
+        this.setGender(_gender);
     }
 
 
-    public int getIdUser() {
-        return idUser;
-    }
-
+    public int getIdUser() { return idUser; }
     public void setIdUser(int idUser) {
         this.idUser = idUser;
     }
 
-    public String getName() {
-        return name;
-    }
-
+    public String getName() { return name; }
     public void setName(String name) {
         if(!name.equals("") && name.length() > 3){
             this.name = name;
@@ -41,10 +35,7 @@ public class User {
 
     }
 
-    public String getPassword() {
-        return password;
-    }
-
+    public String getPassword() { return password; }
     public void setPassword(String password) {
         if(!password.equals("")){
             this.password = password;
@@ -53,13 +44,27 @@ public class User {
         }
     }
 
-    public String getBirth() {
-        return birth;
+    public String getBirth() { return birth; }
+    public void setBirth(String birth) {
+        this.birth = birth;
     }
 
-    public void setBirth(String birth) {
-        //TODO : Formate Date in String
-        this.birth = birth;
+    public String getEmail(){return  email;}
+    public void setEmail(String email){
+        if((email.length() > 0) && (email.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))){
+            this.email = email;
+        }else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public byte getGender(){return gender;}
+    public void setGender(byte gender){
+        if((gender == 0) || (gender == 1)){
+            gender = gender;
+        }else{
+            throw new IllegalArgumentException();
+        }
     }
 
     @NonNull
